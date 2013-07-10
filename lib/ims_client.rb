00158faf59087cb052baf1c2c642a00352d1dfab
Jiren Patel <jiren@joshsoftware.com>
2013-07-10 12:06:18 +0530

    ENV variable for ims server

--- lib/ims_client.rb
+++ lib/ims_client.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class ImsClient
   include HTTParty
-  base_uri(ENV['ims_server'] || 'localhost:3000')
+  base_uri(ENV['IMS_SERVER'] || 'localhost:3000')
 
   def self.dispenser_sync(params = nil)
     params ||= {
