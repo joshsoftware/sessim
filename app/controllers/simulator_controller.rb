22aa5deacdb83bce0c6460802b2741c774afdec5
Jiren Patel <jiren@joshsoftware.com>
2013-07-12 13:13:58 +0530

    Formated Json data and added sample data

--- app/controllers/simulator_controller.rb
+++ app/controllers/simulator_controller.rb
@@ -3,10 +3,10 @@ class SimulatorController < ApplicationController
   before_filter do
     @placeholder = { 'dispense'=>
       [
-        {'project_id'=>'1', 'user_id'=>'E-1', 'plc_id'=>'2', 'dispenser_id'=>'2', 'quantity'=>'10', 'timestamp' => Time.now.to_i},
-        {'project_id'=>'1', 'user_id'=>'E-2', 'plc_id'=>'2', 'dispenser_id'=>'2', 'quantity'=>'10', 'timestamp' => Time.now.to_i}
+        {'project_id'=>'0001', 'user_id'=>'E-1', 'plc_id'=>'0002', 'dispenser_id'=>'0002', 'quantity'=>'10', 'timestamp' => Time.now.to_i},
+        {'project_id'=>'0001', 'user_id'=>'E-2', 'plc_id'=>'0002', 'dispenser_id'=>'0002', 'quantity'=>'10', 'timestamp' => Time.now.to_i}
       ]
-    }.to_json
+    }
   end
 
   def index
